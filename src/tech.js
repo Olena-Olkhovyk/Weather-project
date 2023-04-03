051b89bcd07baf5512bb4c58afb4061ba3f6e7b6
Olena <alyonaeremen@gmail.com>
2023-04-03 14:32:59 +0000

    Update time

--- src/tech.js
+++ src/tech.js
@@ -1,5 +1,26 @@
+function formatDate(timestamp){
+   let date =new Date(timestamp);
+   let hours=date.getHours();
+   if(hours < 10){
+     hours=`0${hours}`
+   }
+   let minutes=date.getMinutes();
+   if(minutes < 10){
+     minutes=`0${minutes}`
+   }
+   let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}, ${hours}:${minutes}`;
+}
 function displayTemperature(response){
-    console.log(response.data);
     let temperatureElement=document.querySelector("#temperature");
     let cityElement=document.querySelector("#city");
     let humidityElement=document.querySelector("#humid");
@@ -11,6 +32,7 @@ function displayTemperature(response){
     cityElement.innerHTML=response.data.city;
     humidityElement.innerHTML=response.data.temperature.humidity;
     windElement.innerHTML=response.data.wind.speed;
+    dateElement.innerHTML=formatDate(response.data.time*1000);
     descriptionElement.innerHTML=response.data.condition.description;
 
 }
